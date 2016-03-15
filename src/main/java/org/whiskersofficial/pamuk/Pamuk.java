166f8060dcc4b2f7e82040d4a734d58f32dacfab
Ecmel B. Canlıer <ecbercnlmc@gmail.com>
2016-03-16 00:02:49 +0200

    Bukkit icin gerekli şeyler eklendi

---
+++ src/main/java/org/whiskersofficial/pamuk/Pamuk.java
@@ -0,0 +1,14 @@
+package org.whiskersofficial.pamuk;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Pamuk extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
