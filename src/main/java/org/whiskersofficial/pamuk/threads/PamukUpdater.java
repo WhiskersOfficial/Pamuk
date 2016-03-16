5bebaf09d123849c1fd747e7bfe95aa7ee93fe15
Ecmel B. Canlıer <ecbercnlmc@gmail.com>
2016-03-16 18:11:14 +0200

    json test

--- src/main/java/org/whiskersofficial/pamuk/threads/PamukUpdater.java
+++ src/main/java/org/whiskersofficial/pamuk/threads/PamukUpdater.java
@@ -1,5 +1,7 @@
 package org.whiskersofficial.pamuk.threads;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.bukkit.Bukkit;
 import org.whiskersofficial.pamuk.util.HTTPUtils;
 
@@ -17,6 +19,11 @@ public class PamukUpdater implements Runnable {
                System.out.println("[Updater] Plugin " + plugins + " is not installed on this server. Updater will skip it.");
             } else {
                 String releasesJSON = HTTPUtils.GETRequest("https://api.github.com/repos/WhiskersOfficial/" + plugins + "/releases");
+
+                Gson gson = new Gson();
+                JsonObject releases = gson.fromJson(releasesJSON, JsonObject.class);
+
+                System.out.println("Plugin: " + plugins + " : " + releases.get("tag_name")); //TEST
             }
         }
     }
