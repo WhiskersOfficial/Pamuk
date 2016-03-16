e83c22f9aaac88e2cae06f1a02317e72a4a5bc5c
Karahan Buhan <hkbuhan@gmail.com>
2016-03-16 17:34:04 +0200

    Color Update

--- src/main/java/org/whiskersofficial/pamuk/Pamuk.java
+++ src/main/java/org/whiskersofficial/pamuk/Pamuk.java
@@ -1,11 +1,24 @@
 package org.whiskersofficial.pamuk;
 
+import org.bukkit.Bukkit;
+import org.whiskersofficial.pamuk.util.Utils;
+
 public class Pamuk {
 
     private PamukPlugin plugin;
 
     protected Pamuk(PamukPlugin plugin) {
         this.plugin = plugin;
+
+        Bukkit.getConsoleSender().sendMessage(color("&7[&fPamuk&7] &bMeow."));
+    }
+
+    public static String color(String message) {
+        return Utils.color(message);
+    }
+
+    public PamukPlugin getPlugin() {
+        return plugin;
     }
 
 }
