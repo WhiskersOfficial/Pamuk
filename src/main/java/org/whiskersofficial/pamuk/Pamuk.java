43be4d1f97d3975281784916744a02f1d307fa8d
Karahan Buhan <hkbuhan@gmail.com>
2016-03-16 17:28:30 +0200

    Utils Update

---
+++ src/main/java/org/whiskersofficial/pamuk/Pamuk.java
@@ -0,0 +1,11 @@
+package org.whiskersofficial.pamuk;
+
+public class Pamuk {
+
+    private PamukPlugin plugin;
+
+    protected Pamuk(PamukPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+}
--- src/main/java/org/whiskersofficial/pamuk/PamukPlugin.java
+++ src/main/java/org/whiskersofficial/pamuk/PamukPlugin.java
@@ -4,11 +4,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class PamukPlugin extends JavaPlugin {
 
+    private static Pamuk api;
+
+    @Override
     public void onEnable() {
+        api = new Pamuk(this);
+    }
+
+    @Override
+    public void onDisable() {
 
     }
 
-    public void onDisable() {
-
+    public static Pamuk getPamuk() {
+        return api;
     }
 }
