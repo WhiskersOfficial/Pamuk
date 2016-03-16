289ddbcb7674d7c4d55d633350e6cea3e2605f3d
Karahan Buhan <hkbuhan@gmail.com>
2016-03-16 18:00:51 +0200

    Config Yaml Added.

--- src/main/java/org/whiskersofficial/pamuk/PamukPlugin.java
+++ src/main/java/org/whiskersofficial/pamuk/PamukPlugin.java
@@ -2,14 +2,18 @@ package org.whiskersofficial.pamuk;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.whiskersofficial.pamuk.threads.PamukUpdater;
+import org.whiskersofficial.pamuk.util.Configuration;
 
 public class PamukPlugin extends JavaPlugin {
 
     private static Pamuk api;
 
+    private static Configuration config;
+
     @Override
     public void onEnable() {
         api = new Pamuk(this);
+        config = new Configuration(this, "config.yml", true);
 
         Thread updaterThread = new Thread(new PamukUpdater(this));
         updaterThread.run();
