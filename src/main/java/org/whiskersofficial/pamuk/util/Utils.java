32b160669c613d9eaa0859639d803a09a0c67635
Karahan Buhan <hkbuhan@gmail.com>
2016-03-16 17:51:39 +0200

    Configuration etc.

---
+++ src/main/java/org/whiskersofficial/pamuk/util/Utils.java
@@ -0,0 +1,11 @@
+package org.whiskersofficial.pamuk.util;
+
+import org.bukkit.ChatColor;
+
+public class Utils {
+
+    public static String color(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+}
