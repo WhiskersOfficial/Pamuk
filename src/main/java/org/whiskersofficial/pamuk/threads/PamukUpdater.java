3f4ecf227f4cfac4feabac0d1d9a582c38dc8ef6
Ecmel B. Canlıer <ecbercnlmc@gmail.com>
2016-03-16 18:51:24 +0200

    dependency and updater complete (HAS TESTING STUFF)

--- src/main/java/org/whiskersofficial/pamuk/threads/PamukUpdater.java
+++ src/main/java/org/whiskersofficial/pamuk/threads/PamukUpdater.java
@@ -1,6 +1,8 @@
 package org.whiskersofficial.pamuk.threads;
 
+import com.github.zafarkhaja.semver.Version;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.bukkit.Bukkit;
 import org.whiskersofficial.pamuk.util.HTTPUtils;
@@ -9,21 +11,33 @@ public class PamukUpdater implements Runnable {
     public void run() {
         System.out.println("[Updater] Updater started...");
 
+        String repoOwner = "Admicos"; //NOTE TO SELF: WhiskersOfficial by default
         String[] pluginsToUpdate = {
-                "Pamuk",
-                "BaskaPlugin"
+             /* "Pamuk" //NOTE TO SELF: Uncomment after testing */
+                "VKick" //NOTE TO SELF: Remove after testing
         };
 
         for (String plugins : pluginsToUpdate) {
             if (Bukkit.getPluginManager().getPlugin(plugins) == null) {
                System.out.println("[Updater] Plugin " + plugins + " is not installed on this server. Updater will skip it.");
             } else {
-                String releasesJSON = HTTPUtils.GETRequest("https://api.github.com/repos/WhiskersOfficial/" + plugins + "/releases");
+                String releasesJSON = HTTPUtils.GETRequest("https://api.github.com/repos/" + repoOwner + "/" + plugins + "/releases");
 
                 Gson gson = new Gson();
-                JsonObject releases = gson.fromJson(releasesJSON, JsonObject.class);
+                JsonArray releases = gson.fromJson(releasesJSON, JsonArray.class);
+                Version latest = Version.valueOf(((JsonObject) releases.get(0)).get("tag_name").toString());
+                Version current = Version.valueOf(Bukkit.getPluginManager()
+                        .getPlugin(plugins)
+                        .getDescription()
+                        .getVersion());
 
-                System.out.println("Plugin: " + plugins + " : " + releases.get("tag_name")); //TEST
+                if (latest.greaterThan(current)) {
+                    System.out.println("[Updater] Plugin " + plugins + " is out of date.");
+                    System.out.println("Latest: " + latest.toString());
+                    System.out.println("Current: " + current.toString());
+                } else {
+                    System.out.println("[Updater] Plugin " + plugins + " is up to date.");
+                }
             }
         }
     }
